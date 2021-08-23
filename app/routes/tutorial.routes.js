f8fc694bea972c8a49128ebedf314807ef3ddc17
rabbitdev1223 <rabbitdev1223@gmail.com>
2021-08-23 15:21:47 +0300

    added: router

---
+++ app/routes/tutorial.routes.js
@@ -0,0 +1,28 @@
+module.exports = (app) => {
+  const tutorials = require("../controllers/tutorial.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  router.post("/", tutorials.create);
+
+  // retrieve all Tutorials
+  router.get("/", tutorials.findAll);
+
+  // Retrieve all published Tutorials
+  router.get("published", tutorials.findallPublished);
+
+  // Retrieve a sing Tutorial with id
+  router.get("/:id", tutorials.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", tutorials.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", tutorials.delete);
+
+  // Delete All Tutorial
+  router.delete("/", tutorials.deleteAll);
+
+  app.use("/api/tutorials", router);
+};
